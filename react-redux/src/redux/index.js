f9c7933a0dc4edcad519ed9a918d31031258458f
Azzaxy <abdurrohmanazis@gmail.com>
2024-05-20 17:34:07 +0700

    feat(middleware) : create redux middleware to set deleteTodo

--- react-redux/src/redux/index.js
+++ react-redux/src/redux/index.js
@@ -1,6 +1,7 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./rootReducer";
+import { todoDeletionCheck } from "./middleware";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(todoDeletionCheck));
 
 export { store };
