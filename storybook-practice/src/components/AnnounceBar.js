f25a5e99de0119d839ff1091544b71d61bed675c
Azzaxy <abdurrohmanazis@gmail.com>
2024-05-25 02:42:41 +0700

    add storybook

--- storybook-practice/src/components/AnnounceBar.js
+++ storybook-practice/src/components/AnnounceBar.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Container from './styled/Container';
-import Title from './styled/Title';
-import Button from './styled/Button';
+import React from "react";
+import PropTypes from "prop-types";
+import Container from "./styled/Container";
+import Title from "./styled/Title";
+import Button from "./styled/Button";
 
 function AnnounceBar({ title, content, ctaLabel, onClick, type }) {
   const backgroundColor = {
-    success: '#4caf50',
-    error: '#f44336',
-    warning: '#ff9800',
-    info: '#2196f3',
-  }
+    success: "#4caf50",
+    error: "#f44336",
+    warning: "#ff9800",
+    info: "#2196f3",
+  };
 
   const textColor = {
-    success: '#fff',
-    error: '#fff',
-    warning: '#fff',
-    info: '#fff',
-  }
-
+    success: "#fff",
+    error: "#fff",
+    warning: "#fff",
+    info: "#fff",
+  };
 
   return (
     <Container
@@ -33,19 +32,25 @@ function AnnounceBar({ title, content, ctaLabel, onClick, type }) {
       <Button
         backgroundColor={textColor[type]}
         textColor={backgroundColor[type]}
-        onClick={onClick}>
-          {ctaLabel}
-      </Button> 
+        onClick={onClick}
+      >
+        {ctaLabel}
+      </Button>
     </Container>
   );
 }
 
 AnnounceBar.propTypes = {
+  /** The title of the announcement */
   title: PropTypes.string.isRequired,
+  /** The content of the announcement */
   content: PropTypes.string.isRequired,
+  /** Action when the CTA button is cli cked  */
   onClick: PropTypes.func.isRequired,
-  type: PropTypes.oneOf(['success', 'error', 'warning', 'info']).isRequired,
+  /** Type of announcement, it will change the color of the bar  */
+  type: PropTypes.oneOf(["success", "error", "warning", "info"]).isRequired,
+  /** The label of the call to action button  */
   ctaLabel: PropTypes.string.isRequired,
-}
+};
 
 export default AnnounceBar;
